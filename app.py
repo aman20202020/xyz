7b666f917045c196ee652e0b7ee5e4b53bcee39e
aman20202020 <53239907+aman20202020@users.noreply.github.com>
2021-06-05 11:35:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import glob
 import re
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
+from PIL import Image
 
 # Keras
 from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
@@ -59,7 +60,7 @@ def prediction():
 	#image = cv2.imread("img.jpg")
 	test_image = image.load_img('img.jpg', target_size = (64, 64))
 	test_image = image.img_to_array(test_image)
-	#image/=255.0
+	image/=255.0
 	test_image = np.expand_dims(test_image, axis = 0)
 	
 
